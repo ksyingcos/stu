462a3848b006c2886b1333193f452ee2e73df232
ShiYing.Ke <ShiYing.Ke@geely.com>
2018-09-12 22:01:42 +0800

    添加swagger2markup

---
+++ stu-spring-boot/stu-spring-boot-swagger2markup/src/main/java/org/stu/swagger2markup/Swagger2MarkupDemo.java
@@ -0,0 +1,26 @@
+package org.stu.swagger2markup;
+
+/**
+ * <strong>Description : </strong> TODO
+ * <br>
+ * <strong>Create on : </strong> 2018/9/11
+ * <br>
+ * <p>
+ *
+ * @author ShiYing.Ke
+ * @since 2.0.0
+ */
+public class Swagger2MarkupDemo {
+
+  public static void main(String[] args) {
+    //    //    输出Ascii格式
+    //    Swagger2MarkupConfig config = new Swagger2MarkupConfigBuilder()
+    //        .withMarkupLanguage(MarkupLanguage.ASCIIDOC)
+    //        .build();
+    //
+    //    Swagger2MarkupConverter.from(new URL("http://127.0.0.1:8201/v2/api-docs?group=%E6%89%80%E6%9C%89%E6%8E%A5%E5%8F%A3"))
+    //        .withConfig(config)
+    //        .build()
+    //        .toFolder(Paths.get("src/docs/asciidoc/generated"));
+  }
+}
